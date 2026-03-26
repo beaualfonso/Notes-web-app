e3692bf4a4ba205b2b4b2d214599d105a416af9f
beau <beau@beaus-MacBook-Pro.local>
2026-03-26 17:08:04 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+//welcome screen
+let welcomeBox = document.getElementById("welcome-screen");
+let welcomeStatus = true;
+welcomeBox.classList.add("welcomeIdle");
+//start up
+let noteSidebar = document.getElementById("notes-sidebar");
+let notesPage = document.getElementById("notes-page");
+
+function startNotes(){
+    welcomeBox.classList.remove("welcomeIdle");
+    welcomeBox.classList.add("welcomeFade");
+    setTimeout(function(){
+        noteSidebar.style.display = 'flex';
+        notesPage.style.display = 'flex';
+        noteSidebar.classList.add("noteSidebarIntro");
+        notesPage.classList.add("notePageIntro");
+    }, 100);
+    setTimeout(function(){
+        welcomeBox.style.display = 'none';
+        noteSidebar.classList.remove("noteSidebarIntro");
+        notesPage.classList.remove("notePageIntro");
+    }, 1000);
+    welcomeStatus = false;
+}
+
+if(welcomeStatus)
+{
+    document.addEventListener('keydown', function(event){
+    if(welcomeStatus && event.keyCode == '32')
+    {
+        startNotes()
+    }
+    });
+    document.addEventListener('mousedown', function(event){
+        if(welcomeStatus && event.button === 0)
+        {
+            startNotes();
+        }
+    });
+}
+
+//clock
+function updateClock()
+{
+    const now = new Date();
+    const hour = now.getHours().toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hour}:${min}`;
+    document.getElementById("clock").innerHTML = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+//hide sidebar function
+function hideSidebar(){
+    noteSidebar.classList.remove("noteSidebarIntro");
+    noteSidebar.classList.add("sideBarOut");
+     notesPage.classList.add("notesPageExpand");
+    setTimeout(function(){
+        noteSidebar.style.display = "none";
+    }, 500);
+}
+
+let welcomeContainer = document.getElementById("note-welcome-group");
+let noteTop = document.getElementById("notes-top");
+let noteTextArea = document.getElementById("notes-content");
+noteTextArea.style.display = 'none';
+noteTop.style.display = 'none';
+
+function newNote(){
+    noteTop.style.display = 'flex';
+    noteTextArea.style.display = 'flex';
+    welcomeContainer.style.display = 'none';
+    noteTop.classList.add("noteTextAreaShow");
+    noteTextArea.classList.add("noteTextAreaShow");
+}
